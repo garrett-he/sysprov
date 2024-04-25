ff465702d2ee53cbb848b839bd82b78efc6ff967
Garrett HE <garrett.he@outlook.com>
2024-04-25 11:27:50 +0800

    feat(cygwin): add module: cygwin_apt-cyg

--- modules/cygwin.sh
+++ modules/cygwin.sh
@@ -5,3 +5,16 @@ module_cygwin_skel() {
 
        cp "$SYSPROV_ROOT"/modules/cygwin/.minttyrc ~
 }
+
+module_cygwin_apt-cyg() {
+    if [[ -z "${SYSPROV_MIRROR_CYGWIN-}" ]]; then
+        utils::read 'cygwin mirror' SYSPROV_MIRROR_CYGWIN
+    fi
+
+    sysprov::git_clone https://github.com/transcode-open/apt-cyg.git /usr/local/apt-cyg SYSPROV_MIRROR_APT_CYG_GIT_REMOTE
+
+    ln -s /usr/local/apt-cyg/apt-cyg /usr/local/bin
+
+    apt-cyg cache /var/cache/cygwin
+    apt-cyg mirror "$SYSPROV_MIRROR_CYGWIN"
+}
